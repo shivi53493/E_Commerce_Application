c0d11bcde5adc3a13e7598c1f1342d4442712cd3
shivi53493 <sk0542759@gmail.com>
2025-03-31 09:52:53 +0530

    Today 31032025 commit

---
+++ cart/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path,include
+
+from . import views
+urlpatterns = [
+    
+    path('',views.cart,name="cart"),
+    path('add_cart/<int:product_id>/',views.add_cart,name='add_cart'),
+    
+
+    
+]
